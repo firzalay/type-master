919ec838f0212a6957327412f43ddb715ddf12b0
Angelo <christianangelor94@gmail.com>
2023-10-09 14:49:25 +0700

    feat : make leaderboard

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,21 +3,38 @@
 
     let name = "";
     let password = "";
+    let errorMessage = "";
 
-
-    
     const submit = async () => {
-        await fetch("http://localhost:8000/api/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            credentials: "include",
-            body: JSON.stringify({
-                name,
-                password,
-            }),
-        });
-        
-        await goto("/");
+        if (!name || !password) {
+            errorMessage = "Silakan isi semua bidang!";
+            return; // Jangan melanjutkan jika ada kesalahan
+        }
+
+        // Menghapus pesan kesalahan jika validasi berhasil
+        errorMessage = "";
+
+        try {
+            const response = await fetch("http://localhost:8000/api/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+                body: JSON.stringify({
+                    name,
+                    password,
+                }),
+            });
+
+            if (response.ok) {
+                await goto("/");
+                // Jika login berhasil, navigasi ke halaman lain di sini (contoh: "/dashboard")
+            } else {
+                // Tangani kesalahan dari server (misalnya, login gagal)
+                errorMessage = "Login gagal. Periksa kembali nama dan kata sandi Anda.";
+            }
+        } catch (error) {
+            errorMessage = "Terjadi kesalahan saat mencoba login.";
+        }
     };
 </script>
 
@@ -25,19 +42,23 @@
     <h1>Login Form</h1>
     <div class="main">
         <div class="name">
-            <label for="">Name</label>
-            <input bind:value={name} type="name" name="" id="" />
+            <label for="name">Name</label>
+            <input bind:value={name} type="text" name="name" id="name" />
         </div>
 
         <div class="password">
-            <label for="">Password</label>
-            <input bind:value={password} type="password" name="" id="" />
+            <label for="password">Password</label>
+            <input bind:value={password} type="password" name="password" id="password" />
         </div>
     </div>
 
     <p>Dont have an account? <a href="/register">Register First!</a></p>
 
     <button type="submit">Submit</button>
+
+    {#if errorMessage}
+        <p class="error">{errorMessage}</p>
+    {/if}
 </form>
 
 <style>
@@ -46,7 +67,6 @@
         justify-content: center;
         align-items: center;
         flex-direction: column;
-
         margin: 20px 0;
     }
 
@@ -72,5 +92,10 @@
 
     p a {
         color: white;
+    }
+
+    .error {
+        color: red;
+        font-size: 14px;
     }
 </style>
